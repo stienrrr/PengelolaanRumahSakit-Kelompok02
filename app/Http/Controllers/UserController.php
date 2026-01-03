04408ca24c70054a73da303e7862d5c8047b9d4b
stienrrr <siti.nsh1806@gmail.com>
2026-01-04 02:45:11 +0700

    add: Users List

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function adminIndex() 
+    {
+        $datas = User::whereHas('roles', function ($query) {
+            $query->whereNotIn('name', ['pasien']);
+        })->get();
+
+        return view('cms.user.admin.index', compact('datas'));
+    }
+
+    public function patientIndex()
+    {
+        $datas = User::whereHas('roles', function ($query) {
+            $query->where('name', ['pasien']);
+        })->get();
+
+        return view('cms.user.patient.index', compact('datas'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'login'])->name('login');
@@ -13,13 +14,9 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::prefix('users')->group(function () {
-        Route::get('/admin', function () {
-            return view('cms.user.admin.index');
-        })->name('users.admin');
+        Route::get('/admin', [UserController::class, 'adminIndex'])->name('users.admin');
 
-        Route::get('/patient', function () {
-            return view('cms.user.patient.index');
-        })->name('users.patient');
+        Route::get('/patient', [UserController::class, 'patientIndex'])->name('users.patient');
     });
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
